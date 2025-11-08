4b461c2bb04dbbc12a9a065e58d341180848207a
Varshu H R <varshuhr22@gmail.com>
2025-11-08 18:26:42 +0530

    Delete models directory

--- models/message.js
+++
@@ -1,7 +0,0 @@
-// backend/models/message.js
-const db = require("../database");
-
-exports.createMessage = (name, email, message, callback) => {
-  const sql = "INSERT INTO messages (name, email, message) VALUES (?, ?, ?)";
-  db.query(sql, [name, email, message], callback);
-};
